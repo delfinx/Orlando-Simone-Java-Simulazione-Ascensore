358b02296666a20ef8db60edfde760b45a6147c0
delfinx <simone.orlando.007@gmail.com>
2025-03-30 11:11:41 +0200

    completate 2 funzioni utili per il Main

--- main.java
+++ main.java
@@ -17,10 +17,11 @@ public class main {
 		listaPersone.add(Ionela);
 		listaPersone.add(Matteo);
 		listaPersone.add(Sergio);
-		/*Ascensore ascensore = new Ascensore(0, 8, listaPersone);
-		// CREATO PIANO1
+		Ascensore ascensore = new Ascensore(0, 8, listaPersone);
+		
 		ArrayList<Persona> codaPersone = new ArrayList<>();
-		Piano piano1 = new Piano(2, codaPersone);*/
+		Piano piano1 = new Piano(2, codaPersone);
+
 		for(Persona persona: listaPersone){
 			int randomDestinazione = 0;
 			do {
@@ -34,17 +35,42 @@ public class main {
 
 		}
 		
-		public void chiamaAscensore(Ascensore ascensore, Persona persona) {
+		public void chiamaAscensore(Ascensore ascensore, Persona persona, Piano piano1, ArrayList<Persona> codaPersone) {
          if (ascensore.getNumeroPersone() < 8) {
              ascensore.apriPorte();
              persona.saliSuAscensore(ascensore, persona);
+			 piano1.rimuoviPersonaCoda(persona);
              ascensore.chiudiPorte();
+			 if(ascensore.getNumeroPersone() < 4){	//L'ASCENSORE VA ALLA DESTINAZIONE DELLA PERSONA SE CI SONO MENO DI 4 PERSONE
+				if(ascensore.getPianoCorrente() < persona.getPianoDestinazione()){
+					ascensore.salita();
+				}
+				else if(ascensore.getPianoCorrente() > persona.getPianoDestinazione()){
+					ascensore.discesa();
+				}
+			 }
          } else {
              System.out.println("L'ascensore è pieno. Attendere che si svuoti.");
+			 codaPersone.add(persona);
          }
      }
 
-	 
+	 	public void controlloCoda(Ascensore ascensore, ArrayList<Persona> listaPersone, Piano piano1, ArrayList<Persona> codaPersone) {
+    for (int i = 0; i < 12; i++) {
+        for (Persona persona : listaPersone) { // Itera sulla lista delle persone
+            if (persona.getPianoCorrente() != ascensore.getPianoCorrente()) {
+                piano1.aggiungiPersonaCoda(persona);
+
+                chiamaAscensore(ascensore, persona, piano1, codaPersone ); // L'ascensore si muove verso la persona
+
+                if (persona.getPianoDestinazione() == ascensore.getPianoCorrente()) {
+                    persona.scendiDaAscensore(ascensore, persona);
+                }
+            }
+        }
+    }
+}
+
 	 }
 		
 	
